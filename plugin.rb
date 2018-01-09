732d895c9fd0edc643def4920f125e9f1ff05886
Alexander Chaychuk <mr.sashich@gmail.com>
2018-01-09 15:48:32 +0300

    use external_id instead of id; add userAuth

--- plugin.rb
+++ plugin.rb
@@ -3,3 +3,27 @@
 # authors: Shoppilot team
 
 # register_asset 'javascripts/discourse/initializers/page-tracking.js.es6'
+
+after_initialize do
+
+  ApplicationHelper.module_eval do
+    def google_universal_analytics_json(ua_domain_name = nil)
+      result = {}
+      if ua_domain_name
+        result[:cookieDomain] = ua_domain_name.gsub(/^http(s)?:\/\//, '')
+      end
+      if current_user.present?
+        result[:userAuth] = 1
+        if (sso_record = current_user.single_sign_on_record)
+          result[:userId] = sso_record.external_id
+        end
+      else
+        result[:userAuth] = 0
+      end
+      if SiteSetting.ga_universal_auto_link_domains.present?
+        result[:allowLinker] = true
+      end
+      result.to_json.html_safe
+    end
+  end
+end
